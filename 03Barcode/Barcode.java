1c6db6cb3249ddae81da2670412c2189327b1e18
Naotaka Kinoshita <naotakakinoshita@gmail.com>
2016-12-10 14:06:31 -0500

    finished testing

--- 03Barcode/Barcode.java
+++ 03Barcode/Barcode.java
@@ -1,19 +1,19 @@
 import java.lang.Integer;
 import java.util.ArrayList;
+
 public class Barcode extends ArrayList<String> implements Comparable<Barcode>{
    private String _zip;
-   private int _checkDigit;
    private String[] codes = {"||:::", ":::||", "::|:|", "::||:", ":|::|", ":|:|:", ":||::", "|:::|", "|::|:", "|:|::"};
-   static private ArrayList<String> c;
-   /*
-   private void thing() {
-       for (int i = 0 ; i < codes.length() ; i ++) {
+
+   static private ArrayList<String> c = new ArrayList<String>();
+   private void thing(String[] codes) {
+       for (int i = 0 ; i < codes.length ; i ++) {
 	   c.add(codes[i]);
        }
    }
-   */
 
    public Barcode(String zip) {
+       thing(codes);
        if (zip.length() != 5) {
 	   throw new IllegalArgumentException("Incorrect length");
        }
@@ -22,16 +22,7 @@ public class Barcode extends ArrayList<String> implements Comparable<Barcode>{
        }catch (NumberFormatException e) {
 	   throw new IllegalArgumentException("bad format");
        }
-       _zip = zip;
-       _checkDigit = checkSum(_zip); 
-   }
-
-   public String zip() {
-       return _zip;
-   }
-
-   public int checkDigit() {
-       return _checkDigit;
+       _zip = zip; 
    }
 
    private static int checkSum(String zip){
@@ -46,7 +37,7 @@ public class Barcode extends ArrayList<String> implements Comparable<Barcode>{
    }
    
    public String toString(){
-       String zip1 = _zip + _checkDigit;
+       String zip1 = _zip + checkSum(_zip);
        String s = zip1 + " |";
        for (int i = 0; i < zip1.length() ; i ++) {
 	   char e = zip1.charAt(i);
@@ -72,19 +63,19 @@ public class Barcode extends ArrayList<String> implements Comparable<Barcode>{
        if (code.length() != 32) {
 	   throw new IllegalArgumentException("Invalid length");
        }
-       if (code.charAt(0) != '|' && code.charAt(31) != '|') {
+       if (code.charAt(0) != '|' || code.charAt(31) != '|') {
 	   throw new IllegalArgumentException("Invalid guardrails");
        }
        for (int i = 0 ; i < code.length() ; i ++) {
-	   if (code.charAt(i) != ':' || code.charAt(i) != '|') {
+	   if (code.charAt(i) != ':' && code.charAt(i) != '|') {
 	       throw new IllegalArgumentException("non-barcode character");
 	   }
        }
        String s = "";
-       for (int i = 0 ; i < 6 ; i ++) {
+       for (int i = 1 ; i < 27 ; i +=5) {
 	   int sizeBefore = s.length();
 	   for (int e = 0; e < c.size() ; e ++) {
-	       if (code.substring(i, i + 6).equals(c.get(e))) {
+	       if (code.substring(i, i + 5).equals(c.get(e))) {
 		   s += e;
 	       }
 	   }		       
@@ -92,15 +83,15 @@ public class Barcode extends ArrayList<String> implements Comparable<Barcode>{
 	       throw new IllegalArgumentException("encoded ints are invalid");
 	   }
        }
-       if (! checkSum("" + (Integer.parseInt(s) - (Integer.parseInt(s) % 10)).equals("" + (Integer.parseInt(s) % 10)))) {
+       if (checkSum(s.substring(0,6)) != (Integer.parseInt(s) % 10)) {
 	   throw new IllegalArgumentException("invalid checksum");
        }
-       return s;
+       return s.substring(0,5);
    }	   
       
    public int compareTo(Barcode other) {
-       int z = Integer.parseInt(_zip + _checkDigit);
-       int o = Integer.parseInt(other.zip() + other.checkDigit());
+       int z = Integer.parseInt(_zip + checkSum(_zip));
+       int o = Integer.parseInt(other._zip + checkSum(other._zip));
        if (z < o) {
 	   return -1;
        }
@@ -111,4 +102,5 @@ public class Barcode extends ArrayList<String> implements Comparable<Barcode>{
 	   return 0;
        }
    }
+
 }
