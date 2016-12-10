de65ff6a16bacf8a4be4c3916c65b0dc76f698f8
Naotaka Kinoshita <naotakakinoshita@gmail.com>
2016-12-09 22:40:22 -0500

    more work on toZip

--- 03Barcode/Barcode.java
+++ 03Barcode/Barcode.java
@@ -1,139 +1,114 @@
 import java.lang.Integer;
-
-public class Barcode implements Comparable<Barcode>{
+import java.util.ArrayList;
+public class Barcode extends ArrayList<String> implements Comparable<Barcode>{
    private String _zip;
    private int _checkDigit;
+   private String[] codes = {"||:::", ":::||", "::|:|", "::||:", ":|::|", ":|:|:", ":||::", "|:::|", "|::|:", "|:|::"};
+   static private ArrayList<String> c;
+   /*
+   private void thing() {
+       for (int i = 0 ; i < codes.length() ; i ++) {
+	   c.add(codes[i]);
+       }
+   }
+   */
 
-  public Barcode(String zip) {
-      if (zip.length() != 5) {
-	  throw new IllegalArgumentException("Incorrect length");
-      }
-      try {
-	  Integer.parseInt(zip);
-      }catch (NumberFormatException e) {
-	  throw new IllegalArgumentException("bad format");
-      }
-      _zip = zip;
-      _checkDigit = checkSum(_zip); 
-  }
+   public Barcode(String zip) {
+       if (zip.length() != 5) {
+	   throw new IllegalArgumentException("Incorrect length");
+       }
+       try {
+	   Integer.parseInt(zip);
+       }catch (NumberFormatException e) {
+	   throw new IllegalArgumentException("bad format");
+       }
+       _zip = zip;
+       _checkDigit = checkSum(_zip); 
+   }
 
-  public String zip() {
-      return _zip;
-  }
+   public String zip() {
+       return _zip;
+   }
 
-  public int checkDigit() {
-      return _checkDigit;
-  }
+   public int checkDigit() {
+       return _checkDigit;
+   }
 
-  private static int checkSum(zip){
-      int tempZ = Integer.parseInt(zip);
-      int sum = 0;
-      int i = 1;
-      while (i < 5) {
-	  sum += (tempZ % 10 ^ i);
-	  i ++;
-      }
-      return sum % 10;
-  }
-     
-  public String toString(){
-      String zip1 = _zip + _checkDigit;
-      String s = zip1 + " |";
-      for (int i = 0; i < zip1.length() ; i ++) {
-	  char e = zip1.charAt(i);
-	  String code;
-	  switch (e) {
-	  case 0: code = "||:::";
-	      break;
-	  case 1: code = ":::||";
-	      break;
-	  case 2: code = "::|:|";
-              break;
-          case 3: code = "::||:";
-              break;
-	  case 4: code = ":|::|";
-	      break;
-	  case 5: code = ":|:|:";
-	      break;
-	  case 6: code = ":||::";
-	      break;
-	  case 7: code = "|:::|";
-	      break;
-	  case 8: code = "|::|:";
-	      break;
-	  case 9: code = "|:|::";
-	      break;
-	  default : code = "";
-	      break;
-	  }
-	  s += code;
-      }
-      s += "|";
-      return s;
-  }
-
-  public static String toCode(String zip) {
-      String s = "|";
-      String zip1 = zip + checkSum(zip);
-      for (int i = 0; i < zip1.length() ; i ++) {
-	  char e = zip1.charAt(i);
-	  String code;
-	  switch (e) {
-	  case 0: code = "||:::";
-	      break;
-	  case 1: code = ":::||";
-	      break;
-	  case 2: code = "::|:|";
-              break;
-          case 3: code = "::||:";
-              break;
-	  case 4: code = ":|::|";
-	      break;
-	  case 5: code = ":|:|:";
-	      break;
-	  case 6: code = ":||::";
-	      break;
-	  case 7: code = "|:::|";
-	      break;
-	  case 8: code = "|::|:";
-	      break;
-	  case 9: code = "|:|::";
-	      break;
-	  default : code = "";
-	      break;
-	  }
-	  s += code;
-      }
-      s += "|";
-      return s;
-  }
-
-  public static String toZip(String code) {
-      if (code.length() != 32) {
-	  throw new IllegalArgumentException("Invalid length");
-      }
-      if (code.charAt(0) != '|' && code.charAt(31) != '|') {
-	  throw new IllegalArgumentException("Invalid guardrails");
-      }
-      String s = ""
-      for (int i = 0 ; i < 6 ; i ++) {
-	  if (code.substring(i, i + 6).equals("||:::") {
-		  s += "0";
-	      }
-	   
+   private static int checkSum(String zip){
+       int tempZ = Integer.parseInt(zip);
+       int sum = 0;
+       int i = 1;
+       while (i < 5) {
+	   sum += (tempZ % 10 ^ i);
+	   i ++;
+       }
+       return sum % 10;
+   }
+   
+   public String toString(){
+       String zip1 = _zip + _checkDigit;
+       String s = zip1 + " |";
+       for (int i = 0; i < zip1.length() ; i ++) {
+	   char e = zip1.charAt(i);
+	   s += codes[Integer.parseInt("" + e)];
+       }
+       s += "|";
+       return s;
+   }
+   
+   public static String toCode(String zip) {
+       String[] codes = {"||:::", ":::||", "::|:|", "::||:", ":|::|", ":|:|:", ":||::", "|:::|", "|::|:", "|:|::"};
+       String s = "|";
+       String zip1 = zip + checkSum(zip);
+       for (int i = 0; i < zip1.length() ; i ++) {
+	   char e = zip1.charAt(i);
+	   s += codes[Integer.parseInt("" + e)];
+       }
+       s += "|";
+       return s;
+   }
+   
+   public static String toZip(String code) {
+       if (code.length() != 32) {
+	   throw new IllegalArgumentException("Invalid length");
+       }
+       if (code.charAt(0) != '|' && code.charAt(31) != '|') {
+	   throw new IllegalArgumentException("Invalid guardrails");
+       }
+       for (int i = 0 ; i < code.length() ; i ++) {
+	   if (code.charAt(i) != ':' || code.charAt(i) != '|') {
+	       throw new IllegalArgumentException("non-barcode character");
+	   }
+       }
+       String s = "";
+       for (int i = 0 ; i < 6 ; i ++) {
+	   int sizeBefore = s.length();
+	   for (int e = 0; e < c.size() ; e ++) {
+	       if (code.substring(i, i + 6).equals(c.get(e))) {
+		   s += e;
+	       }
+	   }		       
+	   if (sizeBefore == s.length()) {
+	       throw new IllegalArgumentException("encoded ints are invalid");
+	   }
+       }
+       if (! checkSum("" + (Integer.parseInt(s) - (Integer.parseInt(s) % 10)).equals("" + (Integer.parseInt(s) % 10)))) {
+	   throw new IllegalArgumentException("invalid checksum");
+       }
+       return s;
+   }	   
       
-// postcondition: compares the zip + checkdigit, in numerical order. 
-  public int compareTo(Barcode other){
-      int z = Integer.parseInt(_zip + _checkDigit);
-      int o = Integer.parseInt(other.zip() + other.checkDigit());
-      if (z < o) {
-	  return -1;
-      }
-      if (z > o) {
-	  return 1;
-      }
-      else {
-	  return 0;
-      }
-  }
+   public int compareTo(Barcode other) {
+       int z = Integer.parseInt(_zip + _checkDigit);
+       int o = Integer.parseInt(other.zip() + other.checkDigit());
+       if (z < o) {
+	   return -1;
+       }
+       if (z > o) {
+	   return 1;
+       }
+       else {
+	   return 0;
+       }
+   }
 }
