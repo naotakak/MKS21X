3abfaa592cb25b229af1d214f53a0aeb514a5a9a
Naotaka Kinoshita <naotakakinoshita@gmail.com>
2016-12-21 20:49:26 -0500

    completed

--- Temperature.java
+++ Temperature.java
@@ -1,12 +1,13 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
-public class Temperature extends JFrame{
+public class Temperature extends JFrame implements ActionListener{
     private Container pane;
 
     private JButton cToF, fToC;
     private JLabel l;
-    private JTextField celsius, farenheit;
+    private JTextField input, result;
  
   //CONSTRUCTOR SETS EVERYTHING UP
     public Temperature() {
@@ -17,27 +18,37 @@ public class Temperature extends JFrame{
 	
 	pane = this.getContentPane();
 	pane.setLayout(new FlowLayout());  //NOTE!! THIS CAN BE CHANGED (see below)
-	
-	b = new JButton("Do Nothing");
-	l = new JLabel("This is AWESOME! (lies)",null,JLabel.CENTER);
-	celsius = new JTextField(12);
-	farenheit = new JTextField(12);
-	pane.add(l);
-	pane.add(b);
-	pane.add(celsius);
-	pane.add(farenheit);
+
+	cToF = new JButton("celsius to farenheit");
+	fToC = new JButton("farenheit to celsius");
+	cToF.addActionListener(this);
+	cToF.setActionCommand("cToF");
+	fToC.addActionListener(this);
+	fToC.setActionCommand("fToC");
+	input = new JTextField(12);
+        result = new JTextField(12);
+	pane.add(input);
+	pane.add(cToF);
+	pane.add(fToC);
+	pane.add(result);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+	if(e.getActionCommand().equals("cToF")) {
+	    result.setText(Double.toString(CtoF(Double.parseDouble(input.getText()))));
+	}
+	else if (e.getActionCommand().equals("fToC")) {
+	    result.setText(Double.toString(FtoC(Double.parseDouble(input.getText()))));
+	}
     }
     
     //MAIN JUST INSTANTIATES + MAKE VISIBLE
     public static void main(String[] args) {
-	Window2 g = new Window2();
+	Temperature g = new Temperature();
 	g.setVisible(true);
     }
-}
+    
     public static double CtoF(double t) {
-	if (t == -40.0) {
-	    return -40.0;
-	}
 	return t * (9.0 / 5.0) + 32.0;
     }
 
